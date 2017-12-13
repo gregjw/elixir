52d41aa9e4fc96d12e0d15cb92d39dcc4b0ef86b
1317421 <g.j.worrall@wlv.ac.uk>
2017-12-13 12:11:13 +0000

    Basic Hand -> Play mechanism

--- src/components/Arena.js
+++ src/components/Arena.js
@@ -4,6 +4,29 @@ import _ from 'lodash';
 
 import Card from './Card.js';
 
+function generateRandomID(){
+  var dictionary =
+  ['a','b','c','d','e','f','g','h','i','j','k','l','m',
+  'n','o','p','q','r','s','t','u','v','w','x','y','z',
+  'A','B','C','D','E','F','G','H','I','J','K','L','M',
+  'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+  '0','1','2','3','4','5','6','7','8','9'];
+
+  var d1 = dictionary[Math.floor(Math.random()*dictionary.length)];
+  var d2 = dictionary[Math.floor(Math.random()*dictionary.length)];
+  var d3 = dictionary[Math.floor(Math.random()*dictionary.length)];
+  var d4 = dictionary[Math.floor(Math.random()*dictionary.length)];
+  var d5 = dictionary[Math.floor(Math.random()*dictionary.length)];
+  var d6 = dictionary[Math.floor(Math.random()*dictionary.length)];
+  var d7 = dictionary[Math.floor(Math.random()*dictionary.length)];
+  var d8 = dictionary[Math.floor(Math.random()*dictionary.length)];
+  var d9 = dictionary[Math.floor(Math.random()*dictionary.length)];
+  var d10 = dictionary[Math.floor(Math.random()*dictionary.length)];
+
+  var generated = d1 + d2 + d3 + d4 + d5 + d6 + d7 + d8 + d9 + d10;
+  return generated;
+}
+
 export default class Arena extends Component {
   constructor(props) {
     super(props);
@@ -29,33 +52,53 @@ export default class Arena extends Component {
     });
   }
 
+  moveToPlay(card){
+    if(card === "black"){
+      let bPlayInterim = this.state.bCardsInPlay;
+      bPlayInterim.push(<Card key={generateRandomID()} player="black" />);
+
+      let bHandInterim = this.state.bCardsInHand;
+      bHandInterim.pop();
+
+      this.setState({
+        bCardsInPlay : bPlayInterim,
+        bCardsInHand : bHandInterim
+      });
+    } else {
+      let wPlayInterim = this.state.wCardsInPlay;
+      wPlayInterim.push(<Card key={generateRandomID()} player="white" />);
+
+      let wHandInterim = this.state.wCardsInHand;
+      wHandInterim.pop();
+
+      this.setState({
+        wCardsInPlay : wPlayInterim,
+        wCardsInHand : wHandInterim
+      });
+    }
+
+    console.log("Moved");
+  }
+
   generateHand(colour){
     var hand = [];
 
     if(colour === "black"){
       _.times(4, i =>
-        hand.push(<Card player="black" onClick={this.moveToPlay("black")}/>)
+        hand.push(
+          <Card key={generateRandomID()}
+          moveToPlay={() => this.moveToPlay("black")}
+          player="black" />)
       );
     } else {
       _.times(4, i =>
-        hand.push(<Card player="white" onClick={this.moveToPlay("white")}/>)
+        hand.push(<Card key={generateRandomID()}
+          moveToPlay={() => this.moveToPlay("white")}
+          player="white" />)
       );
     }
 
     return hand;
-  }
-
-
-  moveToPlay(colour){
-    if(colour === "black"){
-      this.setState({
-        bCardsInPlay : <Card player="black" />
-      });
-    } else {
-      this.setState({
-        wCardsInPlay : <Card player="white" />
-      });
-    }
   }
 
   render() {
@@ -76,7 +119,6 @@ export default class Arena extends Component {
         <div className="black-container mid">
           {this.state.bCardsInPlay}
         </div>
-        
       </div>
     );
   }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,45 +1,21 @@
 import React, { Component } from 'react';
 import '../css/Card.css';
 
-function generateRandomID(){
-  var dictionary =
-  ['a','b','c','d','e','f','g','h','i','j','k','l','m',
-  'n','o','p','q','r','s','t','u','v','w','x','y','z',
-  'A','B','C','D','E','F','G','H','I','J','K','L','M',
-  'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-  '0','1','2','3','4','5','6','7','8','9'];
-
-  var d1 = dictionary[Math.floor(Math.random()*dictionary.length)];
-  var d2 = dictionary[Math.floor(Math.random()*dictionary.length)];
-  var d3 = dictionary[Math.floor(Math.random()*dictionary.length)];
-  var d4 = dictionary[Math.floor(Math.random()*dictionary.length)];
-  var d5 = dictionary[Math.floor(Math.random()*dictionary.length)];
-  var d6 = dictionary[Math.floor(Math.random()*dictionary.length)];
-  var d7 = dictionary[Math.floor(Math.random()*dictionary.length)];
-  var d8 = dictionary[Math.floor(Math.random()*dictionary.length)];
-  var d9 = dictionary[Math.floor(Math.random()*dictionary.length)];
-  var d10 = dictionary[Math.floor(Math.random()*dictionary.length)];
-
-  var generated = d1 + d2 + d3 + d4 + d5 + d6 + d7 + d8 + d9 + d10;
-  return generated;
-}
-
 export default class Card extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       player : this.props.player,
-      unique : generateRandomID(),
       attack : 15,
       health : 20
-    };
+    }
   }
 
   render() {
     return (
-      <div className={this.state.player + "-card"}>
-        <div className="unique">{this.state.unique}</div>
+      <div onClick={this.props.moveToPlay} className={this.state.player + "-card"}>
+        <div className="unique">Card</div>
         <div className="center">
           <div className="attack">{this.state.attack}</div>
           <div className="health">{this.state.health}</div>
