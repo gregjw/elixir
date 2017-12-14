749cf8cd64b801d5ec87d87a10f0f4bec29dd9d5
1317421 <g.j.worrall@wlv.ac.uk>
2017-12-14 17:07:58 +0000

    Hand -> play fully functioning system

--- src/components/Arena.js
+++ src/components/Arena.js
@@ -5,14 +5,40 @@ import _ from 'lodash';
 import Card from './Card.js';
 import logo from '../assets/d20.png';
 
+function generateRandomID(){
+  var dictionary =
+  ['a','b','c','d','e','f','g','h','i','j','k','l','m',
+  'n','o','p','q','r','s','t','u','v','w','x','y','z',
+  'A','B','C','D','E','F','G','H','I','J','K','L','M',
+  'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+  '0','1','2','3','4','5','6','7','8','9'];
+
+  var d1 = dictionary[Math.floor(Math.random()*dictionary.length)];
+  var d2 = dictionary[Math.floor(Math.random()*dictionary.length)];
+  var d3 = dictionary[Math.floor(Math.random()*dictionary.length)];
+  var d4 = dictionary[Math.floor(Math.random()*dictionary.length)];
+  var d5 = dictionary[Math.floor(Math.random()*dictionary.length)];
+  var d6 = dictionary[Math.floor(Math.random()*dictionary.length)];
+  var d7 = dictionary[Math.floor(Math.random()*dictionary.length)];
+  var d8 = dictionary[Math.floor(Math.random()*dictionary.length)];
+  var d9 = dictionary[Math.floor(Math.random()*dictionary.length)];
+  var d10 = dictionary[Math.floor(Math.random()*dictionary.length)];
+
+  var generated = d1 + d2 + d3 + d4 + d5 + d6 + d7 + d8 + d9 + d10;
+  return generated;
+}
+
+function generateName(){
+  var choices =
+  ['Defile','Wrath','Adapt','Valeera','Maex','Neiv','Vox','Eim','Rot'];
+
+  var generated = choices[Math.floor(Math.random()*choices.length)];
+  return generated;
+}
+
 export default class Arena extends Component {
   constructor(props) {
     super(props);
-
-    this.resetGame = this.resetGame.bind(this);
-    this.moveToPlay = this.moveToPlay.bind(this);
-    this.generateHand = this.generateHand.bind(this);
-    this.generateRandomID = this.generateRandomID.bind(this);
 
     this.state = {
       primaryFocus : [],
@@ -25,6 +51,9 @@ export default class Arena extends Component {
       wCardsInHand : [],
       wCardsInPlay : []
     }
+
+    this.generateHand = this.generateHand.bind(this);
+    this.moveToPlay = this.moveToPlay.bind(this);
   }
 
   componentDidMount(){
@@ -34,22 +63,19 @@ export default class Arena extends Component {
     });
   }
 
-  resetGame(){
-    this.setState({
-      bCardsInHand : this.generateHand("black"),
-      wCardsInHand : this.generateHand("white"),
-      bCardsInPlay : [],
-      wCardsInPlay : []
-    })
-  }
-
   moveToPlay(card, current){
     if(card === "black"){
       let bPlayInterim = this.state.bCardsInPlay;
       let bHandInterim = this.state.bCardsInHand;
 
-      bPlayInterim.push(bHandInterim[current]);
-      bHandInterim.splice(current, 1);
+      bHandInterim.map((row, i) => {
+        if(row.key === current){
+          bPlayInterim.push(bHandInterim[i]);
+          bHandInterim.splice(current, 1);
+        }
+
+        return current;
+      });
 
       this.setState({
         bCardsInPlay : bPlayInterim,
@@ -59,70 +85,60 @@ export default class Arena extends Component {
       let wPlayInterim = this.state.wCardsInPlay;
       let wHandInterim = this.state.wCardsInHand;
 
-      wPlayInterim.push(wHandInterim[current]);
-      wHandInterim.splice(current, 1);
+      wHandInterim.map((row, i) => {
+        if(row.key === current){
+          wPlayInterim.push(wHandInterim[i]);
+          wHandInterim.splice(current, 1);
+        }
+
+        return current;
+      });
 
       this.setState({
         wCardsInPlay : wPlayInterim,
         wCardsInHand : wHandInterim
       });
     }
-
-    console.log("Moved " + current);
   }
 
   generateHand(colour){
     var hand = [];
 
     if(colour === "black"){
-      _.times(4, i =>
+      _.times(4, i => {
+        let random = generateRandomID();
         hand.push(
-          <Card key={this.generateRandomID}
-          card = {i}
-          unique = {this.generateRandomID}
-          moveToPlay={() => this.moveToPlay("black", i)}
+          <Card 
+          key = {random}
+          unique = {random}
+          name = {generateName()}
+          moveToPlay={() => this.moveToPlay("black", random)}
+          health = {Math.round(Math.random() * (30 - 10) + 10)}
+          attack = {Math.round(Math.random() * (30 - 10) + 10)}
           player="black" />)
-      );
+      });
     } else {
-      _.times(4, i =>
-        hand.push(<Card key={this.generateRandomID}
-          card = {i}
-          unique = {this.generateRandomID}
-          moveToPlay={() => this.moveToPlay("white", i)}
+      _.times(4, i => {
+        let random = generateRandomID();
+        hand.push(
+          <Card 
+          key = {random}
+          unique = {random}
+          name = {generateName()}
+          moveToPlay={() => this.moveToPlay("white", random)}
+          health = {Math.round(Math.random() * (30 - 10) + 10)}
+          attack = {Math.round(Math.random() * (30 - 10) + 10)}
           player="white" />)
-      );
+      });
     }
 
     return hand;
   }
 
-  generateRandomID(){
-    var dictionary =
-    ['a','b','c','d','e','f','g','h','i','j','k','l','m',
-    'n','o','p','q','r','s','t','u','v','w','x','y','z',
-    'A','B','C','D','E','F','G','H','I','J','K','L','M',
-    'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-    '0','1','2','3','4','5','6','7','8','9'];
-
-    var d1 = dictionary[Math.floor(Math.random()*dictionary.length)];
-    var d2 = dictionary[Math.floor(Math.random()*dictionary.length)];
-    var d3 = dictionary[Math.floor(Math.random()*dictionary.length)];
-    var d4 = dictionary[Math.floor(Math.random()*dictionary.length)];
-    var d5 = dictionary[Math.floor(Math.random()*dictionary.length)];
-    var d6 = dictionary[Math.floor(Math.random()*dictionary.length)];
-    var d7 = dictionary[Math.floor(Math.random()*dictionary.length)];
-    var d8 = dictionary[Math.floor(Math.random()*dictionary.length)];
-    var d9 = dictionary[Math.floor(Math.random()*dictionary.length)];
-    var d10 = dictionary[Math.floor(Math.random()*dictionary.length)];
-
-    var generated = d1 + d2 + d3 + d4 + d5 + d6 + d7 + d8 + d9 + d10;
-    return generated;
-  }
-
   render() {
     return (
       <div className="container center">
-        <img alt="Duel" className="logo" src={logo} onClick={() => this.resetGame()}/>
+        <img alt="Duel" className="logo" src={logo}/>
 
         <div className="white-hand">
           {this.state.wCardsInHand}
--- src/components/Card.js
+++ src/components/Card.js
@@ -5,39 +5,18 @@ export default class Card extends Component {
   constructor(props) {
     super(props);
 
-    this.randomAttack = this.randomAttack.bind(this);
-    this.randomHealth = this.randomHealth.bind(this);
-    this.generateName = this.generateName.bind(this);
-
     this.state = {
-      name : this.generateName(),
-      unique : this.props.unique,
       player : this.props.player,
-      attack : this.randomAttack(),
-      health : this.randomHealth()
+      character : this.props.name,
+      attack : this.props.attack,
+      health : this.props.health
     }
-  }
-
-  generateName(){
-    var choices =
-    ['Defile','Wrath','Adapt','Valeera','Maex','Neiv','Vox','Eim','Rot'];
-
-    var generated = choices[Math.floor(Math.random()*choices.length)];
-    return generated;
-  }
-
-  randomAttack(){
-    return Math.round(Math.random() * (30 - 10) + 10);
-  }
-
-  randomHealth(){
-    return Math.round(Math.random() * (30 - 10) + 10);
   }
 
   render() {
     return (
-      <div onDoubleClick={this.props.moveToPlay} className={this.state.player + "-card"}>
-        <div className="unique">{this.state.unique}</div>
+      <div onClick={this.props.moveToPlay} className={this.state.player + "-card"}>
+        <div className="unique">{this.state.character} [{this.props.unique}]</div>
         <div className="center">
           <div className="attack">{this.state.attack}</div>
           <div className="health">{this.state.health}</div>
