c95798730691cd94d6e5250b49de4dc5f1bf9198
1317421 <g.j.worrall@wlv.ac.uk>
2018-01-02 15:08:18 +0000

    Multiple speech descriptors added

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -7,6 +7,13 @@ function generateName(){
   return choices_array[choice];
 }
 
+function generateDescriptor(){
+  let choices_array = ['croaks','screams','yells','whispers','shouts'];
+  let choice = Math.floor(Math.random()*choices_array.length);
+
+  return choices_array[choice];
+}
+
 export default class Dialog extends Component {
   constructor(props) {
     super(props);
@@ -17,14 +24,15 @@ export default class Dialog extends Component {
       mana : this.props.mana,
       health : this.props.health,
       stamina : this.props.stamina,
-      cost : this.props.cost
+      cost : this.props.cost,
+      descriptor : generateDescriptor()
     }
   }
 
   render() {
     return (
       <div onClick={this.props.restart} className="dialog animated slideInLeft">
-        <div className="descriptor">A {this.state.character} walks in and croaks </div>
+        <div className="descriptor">A {this.state.character} walks in and {this.state.descriptor} </div>
 
         I need a potion with at least <span className="dialog-stat text-mana">{this.state.mana} mana</span> but please, dont exceed <span className="dialog-stat text-agility">{this.state.cost} coins</span>.
         
